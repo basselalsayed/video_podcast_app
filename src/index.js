b8b055c2637bfd02750000ef9ef7b905105aaef4
basselalsayed <alsayed.bassel@gmail.com>
2020-07-01 15:26:31 +0100

    new dependency react-keyboard-navigation

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { NavigationProvider } from 'react-keyboard-navigation';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavigationProvider>
+      <App />
+    </NavigationProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
