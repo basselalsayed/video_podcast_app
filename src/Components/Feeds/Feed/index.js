60a5d59f16ac50501c48d94e6c8dcbc237786384
basselalsayed <alsayed.bassel@gmail.com>
2020-07-02 02:37:42 +0100

    feed component successfully using setNowPlaying hook to change video via context

--- src/Components/Feeds/Feed/index.js
+++ src/Components/Feeds/Feed/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import HoverVideoPlayer from 'react-hover-video-player';
 
 import { ListGroupItem, Card, Spinner } from 'react-bootstrap';
+import { VideoContext } from '../../Video/context';
 import { feed, vidThumb } from './Feed.module.css';
 
-const Feed = ({ feed: { title, link, pubDate } }) => (
-  <ListGroupItem action>
-    <Card className={feed}>
-      <Card.Title>{title}</Card.Title>
-      <Card.Body>
-        <HoverVideoPlayer
-          className={vidThumb}
-          videoSrc={link}
-          loadingOverlay={<Spinner animation='border' />}
-        />
-      </Card.Body>
-      <Card.Footer>{pubDate}</Card.Footer>
-    </Card>
-  </ListGroupItem>
-);
+const Feed = ({ feed: { title, link, pubDate } }) => {
+  const { setNowPlaying } = useContext(VideoContext);
+
+  return (
+    <ListGroupItem action onClick={() => setNowPlaying(link)}>
+      <Card className={feed}>
+        <Card.Title>{title}</Card.Title>
+        <Card.Body>
+          <HoverVideoPlayer
+            className={vidThumb}
+            videoSrc={link}
+            loadingOverlay={<Spinner animation='border' />}
+          />
+        </Card.Body>
+        <Card.Footer>{pubDate}</Card.Footer>
+      </Card>
+    </ListGroupItem>
+  );
+};
 Feed.propTypes = {
   feed: PropTypes.object.isRequired,
 };
