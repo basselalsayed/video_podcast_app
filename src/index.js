c9a17e52b02e1b8087f5550685b842536d5b72b8
basselalsayed <alsayed.bassel@gmail.com>
2020-07-03 13:52:16 +0100

    store extracted out

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import rootReducer from './Store/reducer';
-
-const store = createStore(
-  rootReducer /* preloadedState, */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+import { store } from './Store/store';
 
 ReactDOM.render(
   <React.StrictMode>
